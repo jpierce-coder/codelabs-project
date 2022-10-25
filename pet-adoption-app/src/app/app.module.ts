caa380c4b11be8df6704ee03b388b69b4dd1ce34
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-10-25 06:46:56 -0500

    Add Components, Service & Model

--- pet-adoption-app/src/app/app.module.ts
+++ pet-adoption-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PetManagerComponent } from './pet-manager/pet-manager.component';
+import { AddPetComponent } from './add-pet/add-pet.component';
+import { EditPetComponent } from './edit-pet/edit-pet.component';
+import { ViewPetComponent } from './view-pet/view-pet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PetManagerComponent,
+    AddPetComponent,
+    EditPetComponent,
+    ViewPetComponent
   ],
   imports: [
     BrowserModule,
---
+++ pet-adoption-app/src/app/pet.model.ts
@@ -0,0 +1,7 @@
+export interface pets {
+  name: string;
+  age: number;
+  breed: string;
+  type: string;
+  photo: string;
+}
