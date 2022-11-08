f74dcb8f2dd7c1124eb1ffb2993f1dc50b878437
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-11-07 19:34:26 -0600

    Update to Project

--- to-do-list/src/app/todos/todo-list/todo-list.component.ts
+++ to-do-list/src/app/todos/todo-list/todo-list.component.ts
@@ -4,29 +4,20 @@ import { Todo } from '../todo.model';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-
   todos: Todo[] = [
-    new Todo(
-      'Test ToDo',
-      'This is a test ToDo',
-      '04/01/2023',
-      'John Pierce'
-    ),
+    new Todo('Test ToDo', 'This is a test ToDo', '04/01/2023', 'John P'),
     new Todo(
       'This is also a test ToDo',
       'This is a test description for a ToDo',
       '07/01/2023',
-      'Tiffany Pierce'
-    )
+      'Tiffany P'
+    ),
   ];
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
